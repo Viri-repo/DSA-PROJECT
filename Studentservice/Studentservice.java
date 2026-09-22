ceef3431058680d07d783417de9452bd62ca49c7
Mbeurora Kangootui <kangootuimbeurora@gmail.com>
2026-09-22 05:40:37 +0200

    Add Studentservice

---
+++ Studentservice/Studentservice.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Student {
+    int studentNo;
+    String name;
+    String serviceType;
+    int serviceTime;
+
+    Student(int studentNo, String name, String serviceType, int serviceTime) {
+        this.studentNo = studentNo;
+        this.name = name;
+        this.serviceType = serviceType;
+        this.serviceTime = serviceTime;
+    }
+
+    public String toString() {
+        return studentNo + " , " + name + " , "
+             + serviceType + " , " + serviceTime + " min";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter student number: ");
+        int studentNo = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Enter name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter service type: ");
+        String serviceType = input.nextLine();
+
+        System.out.print("Enter service time: ");
+        int serviceTime = input.nextInt();
+
+        Student student = new Student(studentNo, name, serviceType, serviceTime);
+
+        System.out.println("\nStudent Details:");
+        System.out.println(student);
+    }
+}
